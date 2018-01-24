b636b34ad2413dd4e6795a1e3b4b0e98c5e1ca1c
M.R.Z <zgd1348833@gmail.com>
2018-01-24 01:22:29 +0100

    update vagrant

--- cmd/vagrant.sh
+++ cmd/vagrant.sh
@@ -3,22 +3,35 @@ m.vagrant()
 	op=$1
 	case $op in
 		1|'init')
-			m.log.v vagrant add box base https://github.com/sepetrov/trusty64/releases/download/v0.0.5/trusty64.box
-			vagrant add box base https://github.com/sepetrov/trusty64/releases/download/v0.0.5/trusty64.box
+			m.log.v vagrant box add base https://github.com/sepetrov/trusty64/releases/download/v0.0.5/trusty64.box
+			vagrant box add base https://github.com/sepetrov/trusty64/releases/download/v0.0.5/trusty64.box
+			m.log.v vagrant init
+			vagrant init
 			;;
 		2|'start')
-			m.log.v vagrant up --provider=virtualbox
-			vagrant up --provider=virtualbox
+			m.log.v vagrant up --provider=virtualbox --color
+			vagrant up --provider=virtualbox --color
 			;;
-		*)
+		3|ssh|login)
+			m.log.v vagrant ssh
+			vagrant ssh
+			;;
+		4|halt|stop)
+			m.log.v vagrant halt
+			vagrant halt
+			;;
+		0|h|help|*)
 			m.log.v "
 			HOW TO USE VAGRANT
 =====================================================
+0. help 	- display this help information
 1. init 	- init virtual machine by vagrant image
 2. start 	- start vagrant machine
 3. ssh 		- use ssh to login to the machine
-*. help 	- display this help information
+4. halt|stop - stop this machine
 "
+			m.log.v vagrant $@
+			vagrant $@
 			;;
 	esac
 }
